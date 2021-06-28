b25ecd896e202cc269c0bf538e6fb9955a351cc8
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2021-06-28 12:43:59 +0200

    variables snippets

--- examples/100-foundations/src/main.rs
+++ examples/100-foundations/src/main.rs
@@ -5,8 +5,17 @@ fn add(i: i32, j: i32) -> i32 {
 fn main() {
     let a = 32;
     let b: i32 = 64;
-
     let c = add(a, b);
+
+    println!("{}", c);
+
+    let d = 0i32;
+    let c = a + d;
+
+    println!("{}", c);
+
+    let mut c = b + d;
+    c += a;
 
     println!("{}", c);
 }
