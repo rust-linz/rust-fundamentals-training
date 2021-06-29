6ed676167c4aa19cc8696080855125365a9edb42
Stefan Baumgartner <sbaumg@gmail.com>
2021-06-29 22:13:21 +0200

    update

--- examples/100-foundations/src/main.rs
+++ examples/100-foundations/src/main.rs
@@ -1,11 +1,1 @@
-use rand::Rng;
-
-fn main() {
-    let mut random_numbers = Vec::new();
-
-    for _i in 0..=100 {
-        random_numbers.push(rand::thread_rng().gen_range(0..=100))
-    }
-
-    println!("{:?}", random_numbers);
-}
+fn main() {}
