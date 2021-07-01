3e0832db90b4c3637a41935b7605545dd9436af4
Stefan Baumgartner <sbaumg@gmail.com>
2021-07-01 23:27:08 +0200

    lifetimes

--- examples/100-foundations/src/main.rs
+++ examples/100-foundations/src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+fn get_biggest<'a, 'b, 'c>(n: &'a i32, m: &'b i32) -> &'c i32
+where
+    'a: 'c,
+    'b: 'c,
+{
+    if n > m {
+        n
+    } else {
+        m
+    }
+}
+
+fn main() {
+    let a = 5;
+    let b = 6;
+
+    let biggest = get_biggest(&a, &b);
+    println!("{}", biggest);
+}
